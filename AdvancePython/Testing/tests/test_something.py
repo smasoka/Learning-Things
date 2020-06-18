9cce66baf74e300ee213871aeb394e4d2056b64b
Sakhile Masoka <sakhile.harvey@gmail.com>
2020-06-18 13:54:49 +0200

    unittest mock patch

---
+++ AdvancePython/Testing/tests/test_something.py
@@ -0,0 +1,45 @@
+from unittest import mock, TestCase
+
+from project import something
+
+# Inline
+class TestExamples(TestCase):
+    def setUp(self):
+        self.patcher = mock.patch('project.something.check_output', return_value=b'foo\nbar\n')
+        self.patcher.start()
+
+    def test_print_contents_of_cwd_success(self):
+        actual_result = something.print_content_of_cwd()
+
+        expected_directory = b'foo'
+        self.assertIn(expected_directory, actual_result)
+
+    def tearDown(self):
+        self.patcher.stop()
+
+# # Context Manager
+# class TestExamples(TestCase):
+#     def test_print_contents_of_cwd_success(self):
+#         with mock.patch('project.something.check_output', return_value=b'foo\nbar\n'):
+#             actual_result = something.print_content_of_cwd()
+#
+#         expected_directory = b'foo'
+#         self.assertIn(expected_directory, actual_result)
+#
+# # Decorator Patch
+# class TestExamples(TestCase):
+#     @mock.patch('project.something.check_output', return_value=b'foo\nbar\n')
+#     def test_print_contents_of_cwd_success(self, mock_check_output):
+#         actual_result = something.print_content_of_cwd()
+#
+#         expected_directory = b'foo'
+#         self.assertIn(expected_directory, actual_result)
+#
+# # Original Testing
+# class TestExamples(TestCase):
+#     def test_print_contents_of_cwd_success(self):
+#         # running the actual fucntion
+#         actual_result = something.print_content_of_cwd()
+#
+#         expected_directory = b'tests'
+#         self.assertIn(expected_directory, actual_result)
