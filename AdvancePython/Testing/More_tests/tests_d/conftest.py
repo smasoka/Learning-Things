a850544c0f51b71eed3b7bf8d90087f3b0f0a3aa
Sakhile Masoka <sakhile.harvey@gmail.com>
2020-06-19 22:19:14 +0200

    override parametrized fixture with non-parametrized one and vice versa

---
+++ AdvancePython/Testing/More_tests/tests_d/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+@pytest.fixture(params=['one', 'two', 'three'])
+def parametrized_username(request):
+    return request.param
+
+@pytest.fixture
+def non_parametrized_username(request):
+    return 'username'
