4e5fc6ee05b58980656ab20e03fdf568c837e9d7
Sakhile Masoka <sakhile.harvey@gmail.com>
2020-06-21 10:42:15 +0200

    patch object's asstribute

--- AdvancePython/Testing/Patching/test_my_calendar.py
+++ AdvancePython/Testing/Patching/test_my_calendar.py
@@ -2,7 +2,7 @@ import unittest
 from requests.exceptions import Timeout
 from unittest.mock import patch
 
-from my_calendar import get_holidays
+from my_calendar import requests, get_holidays
 
 # patch as a decorator
 class TestCalendar(unittest.TestCase):
@@ -15,13 +15,21 @@ class TestCalendar(unittest.TestCase):
 
 
 # patch as a context manager
-class TestCase(unittest.TestCase):
+class TestCalendar(unittest.TestCase):
     def test_get_holidays_timeout(self):
         with patch('my_calendar.requests') as mock_requests:
             mock_requests.get.side_effect = Timeout
             with self.assertRaises(Timeout):
                 get_holidays()
                 mock_requests.get.assert_called_once()
+
+
+# patch an object's attrubutes instead of the whole object
+class TestCalendar(unittest.TestCase):
+    @patch.object(requests, 'get', side_effect=Timeout)
+    def test_get_holidays_timeout(self, mock_requests):
+        with self.assertRaises(Timeout):
+            get_holidays()
 
 
 if __name__ == '__main__':
