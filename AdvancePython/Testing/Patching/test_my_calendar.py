60bd69a378677c0f6cb3ebf27db833557a77b968
Sakhile Masoka <sakhile.harvey@gmail.com>
2020-06-21 10:28:54 +0200

    patch as a context manager

--- AdvancePython/Testing/Patching/test_my_calendar.py
+++ AdvancePython/Testing/Patching/test_my_calendar.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 
 from my_calendar import get_holidays
 
-
+# patch as a decorator
 class TestCalendar(unittest.TestCase):
     @patch('my_calendar.requests')
     def test_get_holidays_timeout(self, mock_requests):
@@ -12,6 +12,16 @@ class TestCalendar(unittest.TestCase):
         with self.assertRaises(Timeout):
             get_holidays()
             mock_requests.get.assert_called_once()
+
+
+# patch as a context manager
+class TestCase(unittest.TestCase):
+    def test_get_holidays_timeout(self):
+        with patch('my_calendar.requests') as mock_requests:
+            mock_requests.get.side_effect = Timeout
+            with self.assertRaises(Timeout):
+                get_holidays()
+                mock_requests.get.assert_called_once()
 
 
 if __name__ == '__main__':
