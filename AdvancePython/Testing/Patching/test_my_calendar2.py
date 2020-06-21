c3eaba7464581e341558344dae7f228553b211b4
Sakhile Masoka <sakhile.harvey@gmail.com>
2020-06-21 11:08:39 +0200

    Know where to patch

---
+++ AdvancePython/Testing/Patching/test_my_calendar2.py
@@ -0,0 +1,16 @@
+from unittest.mock import patch
+
+import my_calendar
+
+with patch('my_calendar.is_weekday'):
+    print(my_calendar.is_weekday())
+
+# scope matters
+from my_calendar import is_weekday
+
+with patch('my_calendar.is_weekday'):
+    print(is_weekday()) # prints the actual function. Hierachy!
+
+# to correct this
+with patch('__main__.is_weekday'):
+    print(is_weekday())
